44af0e28c1310b7795efe67081fad3a7d6627ee3
hepeitan <hepeitan@didiglobal.com>
2021-11-23 10:53:54 +0800

    1、增加全局错误定义 2、增加http返回结果定义，并打印返回信息 3、trace_id变更为uuid

--- blog-go/go.mod
+++ blog-go/go.mod
@@ -1,16 +1,19 @@
 module best-practics
 
-go 1.13
+go 1.15
 
 require (
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.7.4
+	github.com/google/uuid v1.3.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.7.0
 	github.com/swaggo/gin-swagger v1.3.3
 	go.uber.org/zap v1.10.0
+	golang.org/x/tools v0.1.7 // indirect
 	gorm.io/driver/mysql v1.2.0
 	gorm.io/gorm v1.22.3
 )
---
+++ blog-go/utils/response/response.go
@@ -0,0 +1,56 @@
+package response
+
+import (
+	"best-practics/common"
+	"best-practics/common/consts"
+	"best-practics/utils/log"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+)
+const (
+	SUCCESS = 0
+)
+
+func Result(c *gin.Context, errCode int, msg string, data interface{}) {
+	ctx := common.GetTraceCtx(c)
+	trace := ctx.Value(consts.TraceKey).(*common.Trace)
+	log.Info("_com_request_out",
+		zap.String("trace_id", trace.TraceId),
+		zap.Int("user_id", trace.UserId),
+		zap.Int("errCode", errCode),
+		zap.String("msg", msg),
+		zap.Any("data", data),
+	)
+	//c.Header("key2020","value2020")  	//可以根据实际情况在头部添加额外的其他信息
+	c.JSON(http.StatusOK, gin.H{
+		"code": errCode,
+		"msg":  msg,
+		"data": data,
+	})
+}
+
+// 将json字符窜以标准json格式返回（例如，从redis读取json、格式的字符串，返回给浏览器json格式）
+func ReturnJsonFromString(Context *gin.Context, httpCode int, jsonStr string) {
+	Context.Header("Content-Type", "application/json; charset=utf-8")
+	Context.String(httpCode, jsonStr)
+}
+
+// 语法糖函数封装
+func Success(c *gin.Context) {
+	Result(c, SUCCESS, "success", "")
+}
+
+func SuccessWithData(c *gin.Context, data interface{}) {
+	Result(c, SUCCESS, "success", data)
+}
+
+func SuccessWithMessage(c *gin.Context,message string) {
+	Result(c, SUCCESS, message, map[string]interface{}{})
+}
+
+// 失败的业务逻辑
+func Fail(c *gin.Context, dataCode int, msg string) {
+	Result(c, dataCode, msg, map[string]interface{}{})
+	c.Abort()
+}
